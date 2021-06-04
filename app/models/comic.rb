e84e0f0bef4ae93113cfa15b1958f18b78010c28
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-06-04 15:34:11 +0900

    アソシエーションの定義

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,2 +1,13 @@
 class Comic < ApplicationRecord
+  belong_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  #空の投稿を保存できないようにする
+  validates :title, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :genre_id, numericality: { other_than: 1 } 
+end
 end
